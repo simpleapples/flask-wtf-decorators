7fa5a26946d2fdb5b00d07251c0ca7d0e358fc1d
simpleapples <zangzhiya@gmail.com>
2018-04-26 11:04:33 +0800

    update setup for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='Flask-WTF-Decorators',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     url='https://github.com/simpleapples/flask-wtf-decorators/',
     author='Zhiya Zang',
